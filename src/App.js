0e3573d20abe2f212db25a81f4541930e117dbe9
KrzysztofHum <k.humienny1996@protonmail.com>
2022-05-27 11:06:15 +0200

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
 import Counter from "./features/counter/Counter";
+import Home from "./Home";
 
 function App() {
   return (
     <main className="App">
-      <Counter />
+      <h1>Welcome to React Redux App!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="counter" element={<Counter />} />
+      </Routes>
     </main>
   );
 }
